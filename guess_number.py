2ce41ba3b921ed1d1bb7490dd0e9698f2dc47cff
shailakhot <95691454+shailakhot@users.noreply.github.com>
2025-02-16 14:57:54 +0530

    Add files via upload

--- guess_number.py
+++ guess_number.py
@@ -1,34 +1,41 @@
 import random
-
 import art
+
+no_of_attempts_hard= 5
+no_of_attempts_easy= 10
 
 def think_no():
   num_chosen = random.randint(1, 100)
   return num_chosen
 
-def guess_no(option,no_chosen):
-   if option == 'easy':
-     no_of_attempts = 10
-   elif option == 'hard':
-     no_of_attempts = 5
+def check_answer(turn, num, answer):
+    if turn == 0 and num != answer:
+        print(f"You've run out of guesses. Refresh the page to run again.")
+    elif num > answer:
+        print(f"too high.")
+        print(f"guess again")
+    elif num < answer:
+        print(f"too low.")
+        print(f"guess again")
+    elif num == answer:
+        print(f"You got it right! The number was {answer}.")
+        turn = 0
+    return turn
+
+
+def game(op, answer):
+   global no_of_attempts
+   if op == 'easy':
+     no_of_attempts = no_of_attempts_easy
+   elif op == 'hard':
+     no_of_attempts = no_of_attempts_hard
    else:
        print("Invalid option")
-
    while not no_of_attempts == 0:
        print(f"You have {no_of_attempts} attempts remaining to guess the number.")
        no_of_attempts -= 1
-       num= int(input("Make a guess:"))
-       if no_of_attempts == 0 and num != no_chosen:
-           print("You've run out of guesses. Refresh the page to run again.")
-       elif num > no_chosen:
-           print(f"too high.")
-           print("guess again")
-       elif num < no_chosen:
-           print(f"too low.")
-           print("guess again")
-       elif num == no_chosen:
-         print(f"You got it right! The number was {no_chosen}.")
-         no_of_attempts = 0
+       num = int(input("Make a guess:"))
+       no_of_attempts = check_answer(no_of_attempts, num, answer)
 
 
 print(art.logo)
@@ -36,4 +43,4 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 no_chosen = think_no()
 option = input("Choose a difficulty. Type 'easy' or 'hard':")
-guess_no(option,no_chosen)
+game(option,no_chosen)
